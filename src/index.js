c1821a2464711c26404271608320e081e6d0a1a8
Ekaterina Pokras <eapeip@mail.ru>
2020-10-12 14:00:37 +0500

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let res = 0;
+if (n >= 0) {
+   let n1 = String(n);
+        res = +(n1.split('').reverse().join('')); 
+} else {
+    let n1 = String((-1)*n);
+console.log(n1);
+    res = (+(n1.split('').reverse().join('')));  
 }
+    
+    return res;
+    }
+
